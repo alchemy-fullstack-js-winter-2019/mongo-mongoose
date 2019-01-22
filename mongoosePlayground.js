754f302094139905cf199e8aa2aae61873543c5c
Cari Palazzolo <cari.pizza@gmail.com>
2019-01-22 14:51:47 -0800

    Query a model finished

--- mongoosePlayground.js
+++ mongoosePlayground.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1:27017/tweets', { useNewUrlParser: true });
 
+// First mongoose model
 const tweetSchema = new mongoose.Schema({
   handle: {
     type: String,
@@ -13,9 +14,23 @@ const tweetSchema = new mongoose.Schema({
     required: true
   }
 });
-
+// create a Tweet model
 const Tweet = mongoose.model('Tweet', tweetSchema);
-
+// Create a new tweet
 Tweet
   .create({ handle: 'caripizza', text: 'dis ma first tweet' })
   .then(createdTweet => console.log(createdTweet));
+
+
+// Query a model ~ use singular version!
+// * Query with find
+Tweet 
+  .find()
+  .then(tweets => console.log(tweets));
+// * Query with findById
+Tweet
+  .create({ handle: 'pizzapizza', text: 'a very first tweet' })
+  .then(createdTweet => {
+    return Tweet.findById(createdTweet._id);
+  })
+  .then(foundTweet => console.log(foundTweet));
