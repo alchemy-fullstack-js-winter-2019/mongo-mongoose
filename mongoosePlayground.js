abe7fdaa58368a594a6d071710db65a55524aea0
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-22 14:46:31 -0800

    created new tweet

--- mongoosePlayground.js
+++ mongoosePlayground.js
@@ -1,5 +1,21 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://172.17.0.i2.27017/tweets', {
-  useNewUrlParser: true
-});
+mongoose.connect('mongodb://127.0.0.1:27017/tweets', {
+  useNewUrlParser: true });
+
+  const tweetSchema = new mongoose.Schema({
+    handle: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    }
+  });
+
+  const Tweet = mongoose.model('Tweet', tweetSchema);
+
+  Tweet
+    .create({ handle: 'abel', text: 'my first tweet' })
+    .then(createdTweet => console.log(createdTweet));
