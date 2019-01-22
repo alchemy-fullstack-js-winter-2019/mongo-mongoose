1f3b71a255cff764f23f3645f132563ec1258861
Cari Palazzolo <cari.pizza@gmail.com>
2019-01-22 15:56:22 -0800

    begin integrating express app, build out connect.js util; server runs!

---
+++ test/app.test.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+require('./lib/utils/connect')();
+const mongoose = require('mongoose');
+
+describe('tweets app', () => {
+  beforeEach(done => {
+    return mongoose.connection.dropDatabase(() => {
+      done();
+    });
+  });
+});
