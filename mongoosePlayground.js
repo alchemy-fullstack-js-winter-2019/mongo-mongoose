5336543536b95238a4bd27f40634eb11d5465368
Aaron Dennis <AMDennis1987@gmail.com>
2019-01-22 14:51:34 -0800

    get update and delete added

--- mongoosePlayground.js
+++ mongoosePlayground.js
@@ -25,3 +25,28 @@ Tweet
 Tweet
   .find()
   .then(tweets => console.log(tweets));
+
+Tweet
+  .create({ handle: 'Aaron', text: 'Hello World!' })
+  .then(createdTweet => {
+    return Tweet.findById(createdTweet._id);
+  })
+  .then(foundTweet => console.log(foundTweet));
+
+Tweet
+  .create({ handle: 'Aaron', text: 'Hello World?' })
+  .then(createdTweet => {
+    return Tweet.findByIdAndUpdate(createdTweet._id, { text: 'hi there' });
+  })
+  .then(updatedTweet => console.log(updatedTweet));
+
+Tweet
+  .create({ handle: 'ryan', text: 'my first tweet' })
+  .then(createdTweet => {
+    return Tweet.findByIdAndUpdate(createdTweet._id, { text: 'hi there' },  { new: true });
+  })
+  .then(updatedTweet => console.log(updatedTweet));
+
+Tweet
+  .findByIdAndDelete('5c479cd5345660da94afdbc4')
+  .then(deleted => console.log(deleted));
