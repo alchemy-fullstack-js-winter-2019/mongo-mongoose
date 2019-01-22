f0d3d300e8924f288c16542357de4ae7e71a2e23
Kate Dameron <katerj33@gmail.com>
2019-01-22 15:08:17 -0800

    added queries to create, find and update tweets

--- mongoosePlayground.js
+++ mongoosePlayground.js
@@ -1,22 +1,43 @@
+/* eslint-disable no-console*/
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017', { useNewUrlParser: true })
+// Connect to database (useNewUrlParser prevents errors)
+mongoose.connect('mongodb://127.0.0.1:27017/tweets', { useNewUrlParser: true });
 
 const tweetSchema = new mongoose.Schema({
-  handle: {
-    type: String,
-    required: true
-  },
-  text: {
-    type: String,
-    required: true
-  }
-})
+    handle: {
+        type: String,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    }
+});
 
 const Tweet = mongoose.model('Tweet', tweetSchema);
 
 Tweet
-.create({ handle: 'kater', text: 'my first tweet' })
-.then(createdTweet => console.log(createdTweet));
+    .create({ handle: 'katerj', text: 'my first tweet' })
+    .then(createdTweet => console.log(createdTweet))
+    .catch(err => console.error(err));
+
+Tweet
+    .find()
+    .then(tweets => console.log(tweets));
+
+Tweet
+    .create({ handle: 'kate', text: 'my first tweet' })
+    .then(createdTweet => {
+        return Tweet.findById(createdTweet._id);
+    })
+    .then(foundTweet => console.log(foundTweet));
+
+Tweet
+    .create({ handle: 'kate', text: 'my first tweet' })
+    .then(createdTweet => {
+        return Tweet.findByIdAndUpdate(createdTweet._id, { text: 'hi there' });
+    })
+    .then(updatedTweet => console.log(updatedTweet));
 
 
