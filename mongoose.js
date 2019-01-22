c40ebbecaf05e3164f74880a945764d75d8d3e85
pickuppatterns <pickup.patterns@gmail.com>
2019-01-22 15:38:04 -0800

    adds boilerplate req

---
+++ mongoose.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/tweets', {
+  useNewUrlParser: true
+});
+
+
+const tweetSchema = new mongoose.Schema({
+  handle: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  }
+});
+
+const Tweet = mongoose.model('Tweet', tweetSchema);
+Tweet
+  .create({ handle: 'Daffy-Duck', text: 'chicken al la kingpin' })
+  .then(createdTweet =>
+    console.log(createdTweet))
+  .catch(err => console.error(err));
+Tweet
+  .find()
+  .then(tweets => console.log(tweets));
+Tweet
+  .create({ handle: 'chicken', text: 'chicken parmesan' })
+  .then(createdTweet => {
+    return Tweet.findById(createdTweet._id)
+  })
+  .then(foundTweet => console.log(foundTweet));
+Tweet
+  .create({ handle: 'chicken', text: 'chicken & dumplease' })
+  .then(createdTweet => {
+    return Tweet.findByIdAndUpdate(createdTweet._id, { text: 'chicken sticks' })
+  })
+  .then(updatedTweet => console.log(updatedTweet));
+  Tweet
+  .create({ handle: 'chicken', text: 'chicken salad' })
+  .then(createdTweet => {
+    return Tweet.findByIdAndUpdate(createdTweet._id, { text: 'chicken noodler' },  { new: true })
+  })
+  .then(updatedTweet => console.log(updatedTweet));
+Tweet
+.findByIdAndDelete('5c47a046f09bb577fc0c3f2d')
+.then(data => console.log(data))
