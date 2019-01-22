09b89b74edc81fa213f938062403246e347f5c29
Cari Palazzolo <cari.pizza@gmail.com>
2019-01-22 14:14:28 -0800

    initial commit with mongoosePlayground.js, runs correctly

---
+++ mongoosePlayground.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/tweets', { useNewUrlParser: true });
+
+const tweetSchema = new mongoose.Schema({
+  handle: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  }
+});
+
+const Tweet = mongoose.model('Tweet', tweetSchema);
+
+Tweet
+  .create({ handle: 'caripizza', text: 'dis ma first tweet' })
+  .then(createdTweet => console.log(createdTweet));
