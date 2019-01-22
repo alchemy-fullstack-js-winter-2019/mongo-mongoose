a73a11faea574dffc0014bc1b500bbd0b7b68a8c
Paige Gorry <paigeegorry@gmail.com>
2019-01-22 14:13:35 -0800

    tweet find, create

--- mongoosePlayground.js
+++ mongoosePlayground.js
@@ -19,4 +19,13 @@ Tweet
   .create({ handle: 'paige', text: 'my first tweet' })
   .then(createdTweet => console.log(createdTweet));
 
+Tweet
+  .find()
+  .then(tweets => console.log(tweets));
 
+Tweet
+  .create({ handle: 'paige', text: 'my second tweet' })
+  .then(createdTweet => {
+    return Tweet.findById(createdTweet._id);
+  })
+  .then(foundTweet => console.log(foundTweet));
