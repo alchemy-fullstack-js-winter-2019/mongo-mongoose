475e553d032102a9b716929de66e1dfb9fc05da1
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-22 15:10:05 -0800

    findByIDAndDelete

--- mongoosePlayground.js
+++ mongoosePlayground.js
@@ -41,3 +41,13 @@ mongoose.connect('mongodb://127.0.0.1:27017/tweets', {
     })
     .then(updatedTweet => console.log(updatedTweet));
 
+  Tweet
+    .create({ handle: 'abel', text: 'my first tweet' })
+    .then(createdTweet => {
+      return Tweet.findByIdAndUpdate(createdTweet._id, { text: 'hi there' })
+    })
+    .then(updatedTweet => console.log(updatedTweet));
+
+  Tweet
+    .findByIdAndDelete(FOUND_ID)
+    .then(deleted => console.log(deleted));
