15ed9f9811148b6d06397e22d571f0737aa0eb7e
Kristin Hortsch <kristinhortsch@gmail.com>
2019-01-22 15:51:39 -0800

    fixed eslint errors

--- mongoosePlayground.js
+++ mongoosePlayground.js
@@ -30,24 +30,24 @@ Tweet
 Tweet
   .create({ handle: 'kristin2', text: 'tweet 2' })
   .then(createdTweet => {
-    return Tweet.findById(createdTweet._id)
+    return Tweet.findById(createdTweet._id);
   })
   .then(foundTweet => console.log(foundTweet));
 
 Tweet 
   .create({ handle: 'ROXY', text: 'cat tweet' })
   .then(createdTweet => {
-    return Tweet.findByIdAndUpdate(createdTweet._id, { text: 'roxy signing out' })
+    return Tweet.findByIdAndUpdate(createdTweet._id, { text: 'roxy signing out' });
   })
   .then(updatedTweet => console.log(updatedTweet));
 
 Tweet
   .create({ handle: 'General Roxy', text: 'forward charge!' })
   .then(createdTweet => {
-    return Tweet.findByIdAndUpdate(createdTweet._id, { text: 'RETREAAAAAAAAT. I REPEAT, RETREAT! FALL BACK' }, { new: true })
+    return Tweet.findByIdAndUpdate(createdTweet._id, { text: 'RETREAAAAAAAAT. I REPEAT, RETREAT! FALL BACK' }, { new: true });
   })
   .then(updatedTweet => console.log(updatedTweet));
 
 Tweet
   .findByIdAndDelete('5c479fd6c40de335002b76d6')
-  .then(deleted => console.log(deleted))
+  .then(deleted => console.log(deleted));
