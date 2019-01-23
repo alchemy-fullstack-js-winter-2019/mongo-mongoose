bdb9e9168b82430dad84ec0d6d396a170f2367bb
Connor Frendt <cjacksonfx@yahoo.com>
2019-01-22 16:47:33 -0800

    updated package.json and other files

--- test/app.test.js
+++ test/app.test.js
@@ -1,11 +1,36 @@
 require('dotenv').config();
 require('./lib/utils/connect')();
+const app = require('../lib/app');
 const mongoose = require('mongoose');
+const request = 
 
 describe('tweets app', () => {
   beforeEach(done => {
     return mongoose.connection.dropDatabase(() => {
       done();
     });
+  });
+
+  it('can create a new tweet', () => {
+    return request(app)
+      .post('/tweets')
+      .send({
+        handle: 'ryan',
+        text: 'hello tweets',
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          handle: 'ryan',
+          text: 'hello tweets',
+          _id: expect.any(String),
+          __v: 0
+        });
+      });
+  });
+
+  it('can get a tweet by id', () => {
+    return request(app)
+      .get()
+      .then()
   });
 });
