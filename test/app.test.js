9bafb88947a3042d2ab4df00a38a52bf16c063f8
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-22 16:30:13 -0800

    boilerplated all tests

--- test/app.test.js
+++ test/app.test.js
@@ -1,11 +1,41 @@
 require('dotenv').config();
-require('./lib/utils/connect')();
-const mongoose = require('mongoose');
+require('../lib/utils/connect')();
+const app = require('../lib/app');
+const mkdirp = require('mkdirp');
+const rimraf = require('rimraf');
+
 
 describe('tweets app', () => {
   beforeEach(done => {
     return mongoose.connection.dropDatabase(() => {
+      mkdirp('./data/tweets', done);
+      done();
+      });
+    });
+    afterEach(done => {
+      rimraf('./data/tweets/*', done);
       done();
     });
   });
-});
+
+  // start tests
+
+  it('creates a tweet', () => {
+
+  });
+
+  it('can find a tweet', () => {
+
+  });
+
+  it('can find a tweet by id', () => {
+
+  });
+
+  it('can find by id and update', () => {
+
+  });
+
+  it('can find by id and delete', () => {
+
+  });
