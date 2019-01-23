788921e14f6db2657c0ae47a975779a25b515d30
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-22 16:36:21 -0800

    createTweet test complete

--- test/app.test.js
+++ test/app.test.js
@@ -3,6 +3,7 @@ require('../lib/utils/connect')();
 const app = require('../lib/app');
 const mkdirp = require('mkdirp');
 const rimraf = require('rimraf');
+const request = require('supertest');
 
 
 describe('tweets app', () => {
@@ -20,8 +21,21 @@ describe('tweets app', () => {
 
   // start tests
 
-  it('creates a tweet', () => {
-
+  it('creates a new tweet', () => {
+    return request(app)
+    .post('/tweets')
+    .send({
+      handle: 'abel',
+      text: 'my first tweet'
+    })
+    .then(res => {
+      expect(res.status).toEqual(200);
+      expect(res.body).toEqual({
+        handle: 'abel',
+        text: 'my first tweet',
+        _id: expect.any(String)
+      });
+    });
   });
 
   it('can find a tweet', () => {
