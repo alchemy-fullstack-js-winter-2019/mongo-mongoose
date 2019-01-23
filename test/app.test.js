7fc1d1bd0a716b6b8e322aa1f8efbf69bff289d4
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-23 13:08:46 -0800

    createUser function added to tests

--- test/app.test.js
+++ test/app.test.js
@@ -6,9 +6,16 @@ const app = require('../lib/app');
 const request = require('supertest');
 const mongoose = require('mongoose');
 const Tweet = require('../lib/models/Tweet');
+const User = require('../lib/models/User');
 
 
 describe('tweets app', () => {
+
+  const createUser = (handle, name, email) => {
+    return User.create({ handle, name, email })
+      .then(user => ({ ...user, _id: user._id.toString() }));
+  };
+
   const createTweet = ((handle, text = 'my first tweet') => {
     return Tweet.create({ handle, text });
   });
