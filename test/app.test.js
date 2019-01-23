3dbef397259c2d7c85380b04e3f8b8d7e4839653
Kristin Hortsch <kristinhortsch@gmail.com>
2019-01-22 17:01:13 -0800

    passing all 5 tests for both users and tweets

--- test/app.test.js
+++ test/app.test.js
@@ -14,6 +14,17 @@ const createTweet = (handle) => {
     .then(res => res.body);
 };
 
+const createUser = (name) => {
+  return request(app)
+    .post('/users')
+    .send({ 
+      handle: 'whatislife',
+      name: name,
+      email: 'test@gmail.com'
+    })
+    .then(res => res.body);
+};
+
 describe('tweets app', () => {
   beforeEach(done => {
     return mongoose.connection.dropDatabase(() => {
@@ -90,6 +101,85 @@ describe('tweets app', () => {
           });
       });
   });
-
-
 });
+
+describe('users app', () => {
+  beforeEach(done => {
+    return mongoose.connection.dropDatabase(() => {
+      done();
+    });
+  });
+
+  beforeEach(done => {
+    createUser('General Roxy');
+    done();
+  });
+
+  it('creates a new user', () => {
+    return request(app)
+      .post('/users')
+      .send({
+        handle: 'ghostrider',
+        name: 'Joe',
+        email: 'testing@gmail.com'
+      })
+      .then(res => {
+        expect(res.body.email).toEqual('testing@gmail.com');
+      });
+  });
+
+  it('can list all the users in the database', () => {
+    const handles = ['roxy1', 'roxy2', 'roxy3', 'roxy4'];
+    return Promise.all(handles.map(createUser))
+      .then(() => {
+        return request(app)
+          .get('/users');
+      })
+      .then(({ body }) => {
+        expect(body).toHaveLength(5);
+      });
+  });
+
+  it('gets a user by id', () => {
+    return createUser('kristin1')
+      .then(createdUser => {
+        return request(app) 
+          .get(`/users/${createdUser._id}`)
+          .then(res => {
+            expect(res.body).toEqual({
+              handle: 'whatislife',
+              name: 'kristin1',
+              email: 'test@gmail.com',
+              _id: expect.any(String),
+              __v: 0
+            });
+          });
+      });
+  });
+
+  it('updates a user with :id and returns the update', () => {
+    return createUser('kristin1')
+      .then(createdUser => {
+        createdUser.name = 'test';
+        return request(app)
+          .patch(`/users/${createdUser._id}`)
+          .send(createdUser);
+      })
+      .then(res => {
+        expect(res.text).toContain('test');
+      });
+  });
+
+  it('deletes a user with :id and returns the delete count', () => {
+    return createUser('siberian tiger')
+      .then((createdUser) => {
+        const id = createdUser._id;
+        return request(app)
+          .delete(`/users/${id}`)
+          .then(res => {
+            expect(res.body).toEqual({ 'deleted': 1 });
+          });
+      });
+  });
+});
+
