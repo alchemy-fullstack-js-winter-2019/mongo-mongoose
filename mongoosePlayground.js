2b0077dd7054ed28c8c48524c9c7464f040642ea
Kate Dameron <katerj33@gmail.com>
2019-01-22 17:09:23 -0800

    find by id and create are passing test

--- mongoosePlayground.js
+++ mongoosePlayground.js
@@ -5,42 +5,42 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/tweets', { useNewUrlParser: true });
 
 const tweetSchema = new mongoose.Schema({
-    handle: {
-        type: String,
-        required: true
-    },
-    text: {
-        type: String,
-        required: true
-    }
+  handle: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  }
 });
 
 const Tweet = mongoose.model('Tweet', tweetSchema);
 
 Tweet
-    .create({ handle: 'katerj', text: 'my first tweet' })
-    .then(createdTweet => console.log('create:', createdTweet))
-    .catch(err => console.error(err));
+  .create({ handle: 'katerj', text: 'my first tweet' })
+  .then(createdTweet => console.log('create:', createdTweet))
+  .catch(err => console.error(err));
 
 Tweet
-    .find()
-    .then(tweets => console.log('all tweets:', tweets));
+  .find()
+  .then(tweets => console.log('all tweets:', tweets));
 
 Tweet
-    .create({ handle: 'kate', text: 'my first tweet' })
-    .then(createdTweet => {
-        return Tweet.findOne(createdTweet._id);
-    })
-    .then(foundTweet => console.log('find one:', foundTweet));
+  .create({ handle: 'kate', text: 'my first tweet' })
+  .then(createdTweet => {
+    return Tweet.findById(createdTweet._id);
+  })
+  .then(foundTweet => console.log('find one:', foundTweet));
 
 Tweet
-    .create({ handle: 'ryan', text: 'my first tweet' })
-    .then(createdTweet => {
-        return Tweet.findOneAndUpdate(createdTweet._id, { text: 'hi there' }, { new: true });
-    })
-    .then(updatedTweet => console.log('update:', updatedTweet));
+  .create({ handle: 'ryan', text: 'my first tweet' })
+  .then(createdTweet => {
+    return Tweet.findByIdAndUpdate(createdTweet._id, { text: 'hi there' }, { new: true });
+  })
+  .then(updatedTweet => console.log('update:', updatedTweet));
 
 Tweet
-    .findOneAndDelete({ _id: '5c47a2755919687b20546698' })
-    .then(deleted => console.log('delete:', deleted));
+  .findByIdAndDelete({ _id: '5c47a2755919687b20546698' })
+  .then(deleted => console.log('delete:', deleted));
 
---
+++ test/app.test.js
@@ -0,0 +1,73 @@
+/* eslint-disable no-console*/
+
+require('dotenv').config();
+require('../lib/utils/connect')();
+const mongoose = require('mongoose');
+const app = require('../lib/app');
+const request = require('supertest');
+
+const makeTweet = (text) => {
+  return request(app)
+    .post('/tweets')
+    .send({
+      handle: 'ladybeard',
+      text: text
+    })
+    .then(res => res.body);
+};
+
+
+describe('tweets app', () => {
+
+  beforeEach(done => {
+    return mongoose.connection.dropDatabase(() => {
+      done();
+    });
+  });
+
+  it('creates a new tweet', () => {
+    return request(app)
+      .post('/tweets')
+      .send({
+        handle: 'ladybeard',
+        text: 'I\'m writing a book on PHIL-osophy'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          handle: 'ladybeard',
+          text: 'I\'m writing a book on PHIL-osophy',
+          _id: expect.any(String),
+          __v: 0
+        });
+      });
+  });
+
+  it('finds tweet by id', () => {
+    return makeTweet('Hello, you know')
+      .then(createdTweet => {
+        return request(app)
+          .get(`/tweets/${createdTweet._id}`)
+          .then(res => {
+            expect(res.body).toEqual({
+              handle: 'ladybeard',
+              text: 'Hello, you know',
+              _id: expect.any(String),
+              __v: 0
+            });
+          });
+      });
+  });
+
+  it('finds by id and update', () => {
+    return makeTweet('Soooo')
+      .then(createdTweet => {
+        return request(app)
+          .patch(`/tweets/${createdTweet._id}`)
+          .send({ text: 'HOOOJ' });
+      })
+      .then(res => {
+        expect(res.body.text).toEqual('HOOOJ');
+      });
+  });
+
+});
