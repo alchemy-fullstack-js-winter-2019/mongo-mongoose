8bfacb87ad68aabdd9c53cd3218e7ed6d6865d06
Shaba9 <shaba92@gmail.com>
2019-01-22 16:54:29 -0800

    playground learning good

---
+++ mongoosePlayground.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/tweets', {
+  useNewUrlParser: true
+});
+
+const tweetSchema = new mongoose.Schema({
+  handle: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  }
+});
+
+const Tweet = mongoose.model('Tweet', tweetSchema);
+
+Tweet
+  .findById('5c47bab120216b6bcf6bd6de')
+  .then(data => console.log('data', data));
