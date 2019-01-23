167d16f052ab8a8cf51817845604362d861b06ab
Lance Owen Merrill <Lanceom@hawaii.edu>
2019-01-22 17:32:36 -0800

    4 methods working

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./lib/app');
 require('dotenv').config();
 require('./lib/utils/connect')();
 
-app.listen(2000, () => {
+app.listen(3000, () => {
     /* eslint-disable-next-line */
-    console.log('listening on port 2000');
+    console.log('listening on port 3000');
 });
--- test/tweets.test.js
+++ test/tweets.test.js
@@ -1,8 +1,20 @@
 require('dotenv').config();
-require('./lib/utils/connect')();
+require('../lib/utils/connect')();
 const request = require('supertest');
 const app = require('../lib/app');
 const mongoose = require('mongoose');
+
+const createTweet = (handle) => {
+    return request(app)
+        .post('/tweets')
+        .send({
+            text: 'KAAACAHHH',
+            handle: handle
+        })
+        .then(res => {
+            return res.body;
+        });
+};
 
 describe('test DB methods/routes', () => {
     beforeEach(done => {
@@ -10,12 +22,52 @@ describe('test DB methods/routes', () => {
             done();
         });
     });
+
+
     it('can post the DB', () => {
         return request(app)
             .post('/tweets')
             .send({ handle: 'KananiBoy', text: 'SUUWOOP' })
             .then(res => {
-                expect(res.body).toContain('KananiBoy');
+                expect(res.text).toContain('KananiBoy');
             });
     });
+
+
+    it('it can find all documents in the DB', () => {
+        const tweetsToCreate = ['YOLO', 'SWAG', 'FROSTYFINGERS420'];
+        return Promise.all(tweetsToCreate.map(createTweet))
+            .then(() => {
+                return request(app)
+                    .get('/tweets');
+            })
+            .then(res => {
+                expect(res.body).toHaveLength(3);
+            });
+    });
+
+    it('can find a document by id', () => {
+        return createTweet('XRPMOON')
+            .then(createdTweet => {
+                return request(app)
+                    .get(`/tweets/${createdTweet._id}`);
+            })
+            .then(res => {
+                expect(res.text).toContain('XRPMOON');
+            });
+    });
+
+    it('can update a document by ID', () => {
+        return createTweet('BTCMOON')
+            .then(createdTweet => {
+                createdTweet.handle = 'NEOMOON';
+                return request(app)
+                    .put(`/tweets/${createdTweet._id}`)
+                    .send(createdTweet);
+            })
+            .then(res => {
+                expect(res.text).toContain('NEOMOON');
+            });
+    });
+    //next it 
 });
