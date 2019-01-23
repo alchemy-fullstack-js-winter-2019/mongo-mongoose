ca62be23807a1bdab805c853269884d2d66a09ee
Connor Frendt <cjacksonfx@yahoo.com>
2019-01-23 09:18:01 -0800

    middleware folder added and updated

--- test/app.test.js
+++ test/app.test.js
@@ -3,8 +3,13 @@ require('../lib/utils/connect')();
 const app = require('../lib/app');
 const mongoose = require('mongoose');
 const request = require('supertest');
+const Tweet = require('../lib/models/Tweet');
 
 describe('tweets app', () => {
+  const createTweet = (handle, text = 'a tweet') => {
+    return Tweet.create({ handle, text });
+  };
+  
   beforeEach(done => {
     return mongoose.connection.dropDatabase(() => {
       done();
@@ -28,9 +33,41 @@ describe('tweets app', () => {
       });
   });
 
-  it('can get a tweet by id', () => {
+  it('find a list of tweets', () => {
+    return Promise.all(['ryan', 'another handle'].map(createTweet))
+      .then(createdTweets => {
+        return request(app)
+          .get('/tweets')
+      })
+      .then(res => {
+        expect(res.body).toHaveLength(2);
+      });
+  });
+
+  it('gets a tweet by id', () => {
+    return createTweet('ryan')
+      .then(createdTweet => {
+        Promise.all([
+          Promise.resolve(createdTweet._id),
+          request(app)
+            .get(`/tweets/${createdTweet._id}`)
+        ]);
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          handle: 'ryan',
+          text: 'a tweet',
+          _id: expect.any(String),
+          __v: 0
+        });
+      });
+  });
+
+  it('errors when a bad id is sent', () => {
     return request(app)
-      .get()
-      .then()
+      .get('/tweets/ (lots of characters)')
+      .then(res => {
+        expect(res.status);
+      });
   });
 });
