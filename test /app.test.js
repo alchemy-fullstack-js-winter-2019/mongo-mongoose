a3e4368562955d3536e1b2a13646afe15a34517f
pickuppatterns <pickup.patterns@gmail.com>
2019-01-23 09:03:12 -0800

    Updates a disconnect from the server

--- test /app.test.js
+++ test /app.test.js
@@ -11,24 +11,27 @@ describe('tweets app', () => {
   }
 
   beforeEach(done => {
-    return mongoose.connection.dropDatabase(() => { 
-    done();
+    return mongoose.connection.dropDatabase(() => {
+      done();
     });
   });
 
   it('creates a new tweet', () => {
     return request(app)
-    .post('/tweets')
-    .send({ handle: 'mike', text: 'my 1st tweet' })
-    .then(res => {
-      // console.log('banana', res.body);
-      expect(res.body).toEqual({
+      .post('/tweets')
+      .send({ handle: 'mike', text: 'my 1st tweet' })
+      .then(res => {
+        // console.log('banana', res.body);
+        expect(res.body).toEqual({
           handle: 'mike',
           text: 'my 1st tweet',
           _id: expect.any(String),
           __v: 0
         });
       });
-    });
-   
   });
+
+});
+afterAll(() => {
+  mongoose.disconnect();
+});
