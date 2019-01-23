cf242c1ebd007c50265eaacc60433cf8c6776718
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-23 11:40:27 -0800

    boilerplating user tests

--- test/user.test.js
+++ test/user.test.js
@@ -5,3 +5,38 @@ const request = require('supertest');
 const mongoose = require('mongoose');
 const User = require('../lib/models/User');
 
+describe('users app', () => {
+  const createUser = ((handle, name, email) => {
+    return User.create({ handle, name, email });
+  });
+
+  beforeEach(done => {
+    return mongoose.connection.dropDatabase(() => {
+      done();
+    });
+  });
+
+  it('creates a new user', () => {
+
+  });
+
+  it('finds a list of users', () => {
+
+  });
+
+  it('finds a user by id', () => {
+
+  });
+
+  it('finds a user by id and updates', () => {
+
+  });
+
+  it('finds a user by id and deletes', () => {
+
+  });
+
+  afterAll((done) => {
+    mongoose.connection.close(done);
+  });
+  
