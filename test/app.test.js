f8b3d270f19eb690e36995a65cb70743a18939b7
avanvalk <drewvv@gmail.com>
2019-01-23 13:18:17 -0800

    added patch / delete

--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,18 @@
 require('dotenv').config();
-require('./lib/utils/connect')();
+require('../connect')();
+const app = require('../lib/app');
 const mongoose = require('mongoose');
 const request = require('supertest');
+
+const createTweet = (handle) => {
+  return request(app)
+    .post('/tweets')
+    .send({
+      handle,
+      text: 'my first tweet'
+    })
+    .then(res => res.body);
+};
 
 describe('tweets app', () => {
   beforeEach(done => {
@@ -13,16 +24,64 @@ describe('tweets app', () => {
     return request(app)
     .post('/tweets')
     .send({
-      handle: 'ryan',
-      text: 'hello tweets'
+      handle: 'drew',
+      text: 'some text'
     })
     .then(res => {
       expect(res.body).toEqual({
-        handle: 'ryan',
-        text: 'hello tweets',
-        _id: expect.any(String).String,
+        handle: 'drew',
+        text: 'some text',
+        _id: expect.any(String),
         __v: 0
+      });
+    });
+  });
+  it('gets a list of tweets', () => {
+    return Promise.all(['drew', 'another handle'].map(createTweet))
+      .then(() => {
+        return request(app)
+          .get('/tweets')
       })
-    })
-  })
-})
+      .then(res => {
+        expect(res.body).toHaveLength(2);
+      });
+  });
+  it('gets a tweet by id', () => {
+    return createTweet('some text')
+    .then(createdTweet => {
+      const _id = createdTweet._id;
+      return request(app)
+      .get(`/tweets/${_id}`)
+      .then(res => {
+      expect(res.body).toEqual({
+        handle: 'some text',
+        text: 'my first tweet',
+        _id,
+        __v: 0
+      });
+    });
+  });
+});
+    it('updates a tweet by id', () => {
+      return createTweet('not updated')
+        .then(createdTweet => {
+          const _id = createdTweet._id;
+          return request(app)
+          .patch(`/tweets/${_id}`)
+          .send({ ...createdTweet, text: 'updated' });
+        })
+          .then(res => {
+          expect(res.body.text).toEqual('updated');
+        });
+    });
+    it('deletes a tweet by id', () => {
+      return createTweet('my tweet')
+        .then(createdTweet => {
+          return request(app)
+            .delete(`/tweets/${createdTweet._id}`);
+        })
+        .then(res => {
+          expect(res.text).toEqual('deleted: true');
+        });
+    });
+});
