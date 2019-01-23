d16d6481a33052d8f7e791ca28fe97f9312fef79
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2019-01-22 17:04:35 -0800

    can findbyId

--- test/app.test.js
+++ test/app.test.js
@@ -4,6 +4,16 @@ const app = require('../lib/app');
 const request = require('supertest');
 const mongoose = require('mongoose');
 
+const createTweet = handle => {
+  return request(app)
+    .post('/tweets')
+    .send({
+      handle: handle,
+      text: 'my tweeter tweets'
+    })
+    .then(res => res.body);
+};
+
 describe('tweets app', () => {
   beforeEach(done => {
     return mongoose.connection.dropDatabase(() => {
@@ -24,9 +34,22 @@ describe('tweets app', () => {
         });
       });
   });
-  // it('finds by id', () => {
-
-  // });
+  it('finds by id', () => {
+    return createTweet('TT')
+      .then(createdTweet => {
+        const id = createdTweet._id;
+        return request(app)
+          .get(`/tweets/${id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          handle: 'TT',
+          text: 'my tweeter tweets',
+          _id: expect.any(String),
+          __v: 0
+        });
+      });
+  });
 
 
 });
