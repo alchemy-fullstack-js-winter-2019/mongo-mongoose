a89fa05e3c143a44d065109c53357056829859a5
Tyler Corbett <thetylercorbett@gmail.com>
2019-01-23 13:27:21 -0800

    post passes for tweets

--- test/app.test.js
+++ test/app.test.js
@@ -3,29 +3,20 @@ require('../lib/utils/connect')();
 const mongoose = require('mongoose');
 const request = require('supertest');
 const app = require('../lib/app');
+const Tweet = require('../lib/models/Tweet');
+const User = require('../lib/models/User');
 
-const createTweet = (handle) => {
-  return request(app)
-    .post('/tweets')
-    .send({
-      handle,
-      text: 'my first tweet'
-    })
-    .then(res => res.body);
+const createTweet = (handle, text = 'a tweet') => {
+  return Tweet.create({ handle, text })
+    .then(tweet => ({ ...tweet, _id: tweet._id.toString() }));
+};
+const createUser = (handle, name, email) => {
+  return User.create({ handle, name, email })
+    .then(user => ({ ...user, _id: user._id.toString() }));
 };
 
-const createUser = (handle) => {
-  return request(app)
-    .post('/users')
-    .send({
-      handle,
-      name: 'Tyler Corbett',
-      email:'thetylercorbett@gmail.com'
-    })
-    .then(res => res.body);
-};
+describe('tweets app', () => {
 
-describe.skip('tweets app', () => {
   beforeEach(done => {
     return mongoose.connection.dropDatabase(() => {
       done();
@@ -41,20 +32,23 @@ describe.skip('tweets app', () => {
         expect(res.body.length).toEqual(3);
       });
   });
-  it('posts a tweet', () => {
-    return request(app)
-      .post('/tweets')
-      .send({
-        handle: 'ryan',
-        text: 'hello tweets'
-      })
-      .then(res => {
-        expect(res.body).toEqual({
-          handle: 'ryan',
-          text: 'hello tweets',
-          _id: expect.any(String),
-          __v: 0
-        });
+  it.only('posts a tweet', () => {
+    return createUser('tyler', 'Tyler Corbett', 'tyler@gmail.com')
+      .then(user => {
+        return request(app)
+          .post('/tweets')
+          .send({
+            handle: user._id,
+            text: 'hello tweets'
+          })
+          .then(res => {
+            expect(res.body).toEqual({
+              handle: expect.any(String),
+              text: 'hello tweets',
+              _id: expect.any(String),
+              __v: 0
+            });
+          });
       });
   });
   it('finds a tweet by id', () => {
@@ -110,90 +104,3 @@ describe.skip('tweets app', () => {
   });
 });
 
-
-
-
-describe('users app', () => {
-  beforeEach(done => {
-    return mongoose.connection.dropDatabase(() => {
-      done();
-    });
-  });
-  it('returns a list of users', () => {
-    return Promise.all(['ryan', 'tyler', 'jack'].map(createUser))
-      .then(() => {
-        return request(app)
-          .get('/users');
-      })
-      .then(res => {
-        expect(res.body.length).toEqual(3);
-      });
-  });
-  it('posts a tweet', () => {
-    return request(app)
-      .post('/tweets')
-      .send({
-        handle: 'ryan',
-        text: 'hello tweets'
-      })
-      .then(res => {
-        expect(res.body).toEqual({
-          handle: 'ryan',
-          text: 'hello tweets',
-          _id: expect.any(String),
-          __v: 0
-        });
-      });
-  });
-  it('finds a tweet by id', () => {
-    return createTweet('tyler')
-      .then(createdTweet => {
-        const _id = createdTweet._id;
-        return request(app)
-          .get(`/tweets/${_id}`)
-          .then(res => {
-            expect(res.body).toEqual({
-              handle: 'tyler',
-              text: 'my first tweet',
-              _id,
-              __v: 0
-            });
-          });
-      });
-  });
-  it('updates an existing tweet by id', () => {
-    return createTweet('tyler')
-      .then(createdTweet => {
-        const _id = createdTweet._id;
-        return request(app)
-          .patch(`/tweets/${_id}`)
-          .send({
-            text: 'This tweet is updated'
-          })
-          .then(res => {
-            expect(res.body).toEqual({
-              handle: 'tyler',
-              text: 'This tweet is updated',
-              _id,
-              __v: 0
-            });
-          });
-      });
-  });
-  it('deletes a tweet by id', () => {
-    return createTweet('tyler')
-      .then(createdTweet => {
-        const _id = createdTweet._id;
-        return request(app)
-          .delete(`/tweets/${_id}`)
-          .then(res => {
-            expect(res.body).toEqual({
-              _id,
-              handle: 'tyler',
-              text: 'my first tweet',
-              __v: 0
-            });
-          });
-      });
-  });
-});
