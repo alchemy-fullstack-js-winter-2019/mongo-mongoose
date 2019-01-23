2d01b4ffbe45d928efcdff022cb6588806a3a0b7
pickuppatterns <pickup.patterns@gmail.com>
2019-01-23 09:22:23 -0800

    updates find method test and implementation

--- test /app.test.js
+++ test /app.test.js
@@ -11,27 +11,36 @@ describe('tweets app', () => {
   }
 
   beforeEach(done => {
-    return mongoose.connection.dropDatabase(() => {
-      done();
+    return mongoose.connection.dropDatabase(() => { 
+    done();
     });
   });
 
   it('creates a new tweet', () => {
     return request(app)
-      .post('/tweets')
-      .send({ handle: 'mike', text: 'my 1st tweet' })
-      .then(res => {
-        // console.log('banana', res.body);
-        expect(res.body).toEqual({
+    .post('/tweets')
+    .send({ handle: 'mike', text: 'my 1st tweet' })
+    .then(res => {
+      // console.log('banana', res.body);
+      expect(res.body).toEqual({
           handle: 'mike',
           text: 'my 1st tweet',
           _id: expect.any(String),
           __v: 0
         });
       });
+    });
+    it('finds all the tweets', () => {
+      return Promise.all(['fannyserverpackets', 'another handle'].map(createTweet))
+        .then(createdTweets => {
+          return request(app)
+            .get('/tweets')
+        })
+        .then(res => {
+          expect(res.body).toHaveLength(2);
+        });
+    });
+    afterAll(() => {
+      mongoose.disconnect();
   });
-
-});
-afterAll(() => {
-  mongoose.disconnect();
-});
+  });
