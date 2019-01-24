b22b4eae3d973456ab336f3bf5cbfe6ac5b591fa
cruzayye <johnnyc010392@gmail.com>
2019-01-23 16:15:50 -0800

    find by id and update works

--- test/app.test.js
+++ test/app.test.js
@@ -4,49 +4,59 @@ require('../lib/utils/connect')();//connects to db
 const app = require('../lib/app.js');
 const mongoose = require('mongoose');
 const Tweet = require('../lib/models/Tweet');
+const User = require('../lib/models/Users');
 //whats the difference between res.body // res.text 
 //why did we have to use:
 //return request(app) in the express lab?
-const createTweet = (handle, text = 'this is a sample tweet') => {
-  return Tweet.create({ handle, text });
-};
+
+//linking user and tweet.
+//we need a user to create a tweet.
+//handle is a reference to a user. 
 
 describe('tweets app', () => {
+  const createUser = (handle, name, email) => {
+    return User.create({ handle, name, email })
+    //takes everything in user, then tagetes id to stringify
+      .then(user => ({ ...user, _id: user._id.toString() }));
+  };
+
+
+  const createTweet = (handle, text = 'a tweet') => {
+    // create user first
+    return createUser(handle, 'ryan', 'ryan@email.com')
+      .then(user => {
+        //we pass user._id bc be care linking tweet to user with id.
+        return Tweet.create({ handle: user._id, text })
+          .then(tweet => ({ ...tweet, _id: tweet._id.toString() }));
+      });
+
+  };
   beforeEach(done => {
     return mongoose.connection.dropDatabase(() => {
       done();
     });
   });
-  it('creates a tweet', ()=> {
-    return request(app)
-      .post('/tweets')
-      .send({ handle: 'johnny', text: 'My first tweet to mongo db' })
-      .then(res => {
-        expect(res.body).toEqual({ 
-          handle: 'johnny', 
-          text: 'My first tweet to mongo db',
-          _id: expect.any(String),
-          __v: 0 }); 
+  it('can create a new tweet', () => {
+    return createUser('ryan', 'ryan', 'ryan@ryan.com')
+      .then(user => {
+        return request(app)
+          .post('/tweets')
+          .send({
+            handle: user._id,
+            text: 'hello tweets'
+          })
+          .then(res => {
+            expect(res.body).toEqual({
+              handle: expect.any(String),
+              text: 'hello tweets',
+              _id: expect.any(String),
+              __v: 0
+            });
+          });
       });
+
   });
-  it('gets a tweet by id', () => {
-    return createTweet('mine')
-      .then(createdTweet => {
-        return Promise.all([
-          Promise.resolve(createdTweet._id),
-          request(app)
-            .get(`/tweets/${createdTweet._id}`)
-        ]);
-      })
-      .then(([_id, res]) => {
-        expect(res.body).toEqual({
-          handle: 'mine',
-          text: 'this is a sample tweet',
-          _id,
-          __v: 0
-        });
-      });
-  });
+
   it('gets a list of tweets', ()=> {
     //use promaise all to imply createTweet function
     return Promise.all(['tweet 1', 'tweet 2', 'tweet 3'].map(createTweet))
@@ -61,20 +71,53 @@ describe('tweets app', () => {
       });
 
   });
-  it('updates tweet by id', ()=> {
-    //because this is async...
-    return createTweet('this is a tweet')
+  it('gets a tweet by id', () => {
+    return createTweet('ryan')
       .then(createdTweet => {
-        createdTweet.handle = 'something else';
-        return request(app)
-          .patch(`/tweets/${createdTweet._id}`)
-          .send(createdTweet);
+        return Promise.all([
+          Promise.resolve(createdTweet._id),
+          request(app)
+            .get(`/tweets/${createdTweet._id}`)
+        ]);
       })
-      .then(res => {
-        expect(res.text).toContain('something else');
+      .then(([_id, res]) => {
+        expect(res.body).toEqual({
+          handle: expect.any(Object),
+          text: 'a tweet',
+          _id,
+          __v: 0
+        });
       });
   });
-  it.only('finds by id and deletes', ()=> {
+  it.only('updates tweet by id', ()=> {
+    return createTweet('this is a tweet', 'more', 'another')
+      .then(createdTweet => {
+        return Promise.all([
+          Promise.resolve(createdTweet._id),
+          request(app)
+            .patch(`/tweets/${createdTweet._id}`)
+            .send({ text: 'newTweet' })
+        ])
+          .then(([_id, res]) => {
+            expect(res.body.text).toEqual('newTweet');
+
+          });
+      });
+  });
+  // it.only('updates tweet by id', ()=> {
+  //   //because this is async...
+  //   return createTweet('this is a tweet')
+  //     .then(createdTweet => {
+  //       createdTweet.handle = 'something else';
+  //       return request(app)
+  //         .patch(`/tweets/${createdTweet._id}`)
+  //         .send(createdTweet);
+  //     })
+  //     .then(res => {
+  //       expect(res.text).toContain('something else');
+  //     });
+  // });
+  it('finds by id and deletes', ()=> {
     return createTweet('delete this')
       .then(tweet2Delete => {
         return request(app)
---
+++ test/users.xtest.js
@@ -0,0 +1,37 @@
+const request = require('supertest');
+require('dotenv').config();//module for configuration
+require('../lib/utils/connect')();//connects to db
+const app = require('../lib/app.js');
+const mongoose = require('mongoose');
+const userApp = require('../lib/models/Users');
+
+const creatUser = (handle, name = 'johnny', email = 'email.com') => {
+  return User.create({ handle, name, email });
+};
+
+describe('creates a user', () => {
+  beforeEach(done => {
+    return mongoose.connection.dropDatabase(() => {
+      done();
+    });
+  });
+  it.only('creates a user', ()=> {
+    return request(app)
+      .post('/users')
+      .send({ 
+        handle: 'handle',
+        name: 'johnny',
+        email: 'email.com'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          handle: 'handle',
+          name: 'johnny',
+          email: 'email.com',
+          _id: expect.any(String),
+          __v: 0
+        });
+      });
+  });
+
+});
