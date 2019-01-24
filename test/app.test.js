a9deb6553ebe39d0ac7f957a6d3b1b781a558be9
Cari Palazzolo <cari.pizza@gmail.com>
2019-01-23 21:52:23 -0800

    cleanup tests, praying to travis gods

--- test/app.test.js
+++ test/app.test.js
@@ -41,216 +41,227 @@ describe('tweets app', () => {
   afterAll(done => {
     createTweet('seedDataHandleA', 'seedDataTextA');
     createUser('seedDataHandleB', 'seedDataNameB', 'seedDataEmailB');
-    // mongoose.disconnect();
+    mongoose.disconnect();
     done();
   });
 
   // POST ------------------------------------------
-  it('can create a new tweet', () => {
-    return createUser('2cool', 'Bubs', 'email@e.com')
-      .then(createdUser => {
-        return request(app)
-          .post('/tweets')
-          .send({
-            handle: createdUser._id,
-            text: 'hiya tweety'
-          })
-          .then(res => {
-            expect(res.body).toEqual({
+  describe('POST', () => {
+    it('can create a new tweet', () => {
+      return createUser('2cool', 'Bubs', 'email@e.com')
+        .then(createdUser => {
+          return request(app)
+            .post('/tweets')
+            .send({
               handle: createdUser._id,
-              text: 'hiya tweety',
-              _id: expect.any(String),
-              __v: 0
+              text: 'hiya tweety'
+            })
+            .then(res => {
+              expect(res.body).toEqual({
+                handle: createdUser._id,
+                text: 'hiya tweety',
+                _id: expect.any(String),
+                __v: 0
+              });
             });
-          });
-      });
-  });
-  it('can create a tweet with a random quote', () => {
-    return createUser('evenCooler', 'Bertha', 'bye@e.com')
-      .then(createdUser => {
-        return request(app)
-          .post('/tweets?random=true')
-          .send({
-            handle: createdUser._id
-          })
-          .then(res => {
-            expect(res.body).toEqual({
-              handle: createdUser._id,
-              text: 'I wanna punch you in the face so bad right now.',
-              _id: expect.any(String),
-              __v: 0
+        });
+    });
+    it('can create a tweet with a random quote', () => {
+      return createUser('evenCooler', 'Bertha', 'bye@e.com')
+        .then(createdUser => {
+          return request(app)
+            .post('/tweets?random=true')
+            .send({
+              handle: createdUser._id
+            })
+            .then(res => {
+              expect(res.body).toEqual({
+                handle: createdUser._id,
+                text: 'I wanna punch you in the face so bad right now.',
+                _id: expect.any(String),
+                __v: 0
+              });
             });
-          });
-      });
-  });
-  it('can create a new user', () => {
-    return request(app)
-      .post('/users')
-      .send({
-        handle: 'newcooluser',
-        name: 'Cool Person McGee',
-        email: 'cool@cool.com'
-      })
-      .then(res => {
-        expect(res.body).toEqual({
+        });
+    });
+    it('can create a new user', () => {
+      return request(app)
+        .post('/users')
+        .send({
           handle: 'newcooluser',
           name: 'Cool Person McGee',
-          email: 'cool@cool.com',
-          _id: expect.any(String),
-          __v: 0
+          email: 'cool@cool.com'
+        })
+        .then(res => {
+          expect(res.body).toEqual({
+            handle: 'newcooluser',
+            name: 'Cool Person McGee',
+            email: 'cool@cool.com',
+            _id: expect.any(String),
+            __v: 0
+          });
         });
-      });
+    });
   });
 
   // GET ------------------------------------------
-  it('can get a list of tweets from db', () => {
-    const tweetsToCreate = ['hey', 'hi', 'hello', 'hola'];
-    return Promise.all(tweetsToCreate.map(createTweet))
-      .then(() => {
-        return request(app)
-          .get('/tweets');
-      })
-      .then(({ body }) => {
-        expect(body).toHaveLength(4);
-      });
-  });
-  it('can get a list of users from db', () => {
-    return request(app)
-      .post('/users')
-      .send({
-        handle: 'handleA',
-        name: 'nameA',
-        email: 'emailA'
-      })
-      .then(() => {
-        return request(app)
-          .post('/users')
-          .send({
-            handle: 'handleB',
-            name: 'nameB',
-            email: 'emailB'
-          });
-      })
-      .then(() => {
-        return request(app)
-          .get('/users');
-      })
-      .then(({ body }) => {
-        expect(body).toHaveLength(2);
-      });
+  describe('GET', () => {
+    it('can get a list of tweets from db', () => {
+      const tweetsToCreate = ['hey', 'hi', 'hello', 'hola'];
+      return Promise.all(tweetsToCreate.map(createTweet))
+        .then(() => {
+          return request(app)
+            .get('/tweets');
+        })
+        .then(({ body }) => {
+          expect(body).toHaveLength(4);
+        });
+    });
+    it('can get a list of users from db', () => {
+      return request(app)
+        .post('/users')
+        .send({
+          handle: 'handleA',
+          name: 'nameA',
+          email: 'emailA'
+        })
+        .then(() => {
+          return request(app)
+            .post('/users')
+            .send({
+              handle: 'handleB',
+              name: 'nameB',
+              email: 'emailB'
+            });
+        })
+        .then(() => {
+          return request(app)
+            .get('/users');
+        })
+        .then(({ body }) => {
+          expect(body).toHaveLength(2);
+        });
+    });
   });
 
   // GET by id
-  it('can get a tweet by id', () => {
-    return createTweet('hayyyyyy')
-      .then(createdTweet => {
-        const _id = createdTweet._id;
-        return request(app)
-          .get(`/tweets/${_id}`)
-          .then(res => {
-            expect(res.body).toEqual({
-              handle: {
-                handle: 'hayyyyyy'
-              },
-              text: 'hi I a tweet',
-              _id
+  describe('GET by :id', () => {
+    it('can get a tweet by id', () => {
+      return createTweet('hayyyyyy')
+        .then(createdTweet => {
+          const _id = createdTweet._id;
+          return request(app)
+            .get(`/tweets/${_id}`)
+            .then(res => {
+              expect(res.body).toEqual({
+                handle: {
+                  handle: 'hayyyyyy'
+                },
+                text: 'hi I a tweet',
+                _id
+              });
             });
-          });
-      });
-  });
-  it('can get a user by id', () => {
-    return createUser(
-      '2cool4skool', 'Michael MacDonald', 'smoothjams@hotmail.com'
-    )
-      .then(createdUser => {
-        const _id = createdUser._id;
-        return request(app)
-          .get(`/users/${_id}`)
-          .then(res => {
-            expect(res.body).toEqual({
-              handle: '2cool4skool',
-              name: 'Michael MacDonald',
-              email: 'smoothjams@hotmail.com',
-              _id,
-              __v: 0
+        });
+    });
+    it('can get a user by id', () => {
+      return createUser(
+        '2cool4skool', 'Michael MacDonald', 'smoothjams@hotmail.com'
+      )
+        .then(createdUser => {
+          const _id = createdUser._id;
+          return request(app)
+            .get(`/users/${_id}`)
+            .then(res => {
+              expect(res.body).toEqual({
+                handle: '2cool4skool',
+                name: 'Michael MacDonald',
+                email: 'smoothjams@hotmail.com',
+                _id,
+                __v: 0
+              });
             });
-          });
-      });
+        });
+    });
   });
 
   // PATCH ------------------------------------------
-  it('can retrieve a tweet by :id and update it', () => {
-    const newTweet = {
-      text: 'newText'
-    };
-    return createTweet('flanel', 'blahblah', 'e@e.com')
-      .then(createdTweet => {
-        const _id = createdTweet._id;
-        return request(app)
-          .patch(`/tweets/${_id}`)
-          .send(newTweet);
-      })
-      .then(res => {
-        expect(res.body).toEqual({
-          _id: expect.any(String),
-          handle: {
-            handle: 'flanel'
-          },
-          text: 'newText'
+  describe('PATCH  by :id', () => {
+    it('can retrieve a tweet by :id and update it', () => {
+      const newTweet = {
+        text: 'newText'
+      };
+      return createTweet('flanel', 'blahblah', 'e@e.com')
+        .then(createdTweet => {
+          const _id = createdTweet._id;
+          return request(app)
+            .patch(`/tweets/${_id}`)
+            .send(newTweet);
+        })
+        .then(res => {
+          expect(res.body).toEqual({
+            _id: expect.any(String),
+            handle: {
+              handle: 'flanel'
+            },
+            text: 'newText'
+          });
         });
-      });
-  });
-  it('can retrieve a user by :id and update only name and email', () => { 
-    const newUser = { name: 'booboo head', email: 'b@b.biz' };
-    return createUser(
-      'Baby Pie', 'Angel Face', 'angel@heaven.com'
-    )
-      .then(createdUser => {
-        const _id = createdUser._id;
-        return request(app)
-          .patch(`/users/${_id}`)
-          .send(newUser);
-      })
-      .then(res => {
-        expect(res.body).toEqual({
-          _id: expect.any(String),
-          handle: 'Baby Pie',
-          name: 'booboo head',
-          email: 'b@b.biz',
-          __v: 0
+    });
+
+    it('can retrieve a user by :id and update only name and email', () => { 
+      const newUser = { name: 'booboo head', email: 'b@b.biz' };
+      return createUser(
+        'Baby Pie', 'Angel Face', 'angel@heaven.com'
+      )
+        .then(createdUser => {
+          const _id = createdUser._id;
+          return request(app)
+            .patch(`/users/${_id}`)
+            .send(newUser);
+        })
+        .then(res => {
+          expect(res.body).toEqual({
+            _id: expect.any(String),
+            handle: 'Baby Pie',
+            name: 'booboo head',
+            email: 'b@b.biz',
+            __v: 0
+          });
         });
-      });
+    });
   });
 
   // DELETE ------------------------------------------
-  it('can retrieve a tweet by :id, delete, and return the delete count', () => {
-    return createTweet('alo')
-      .then(createdTweet => {
-        const _id = createdTweet._id;
-        return request(app)
-          .delete(`/tweets/${_id}`)
-          .then(res => {
-            expect(res.body).toEqual({ deleted: 1 });
-            return Tweet.findById(_id)
-              .then(res => {
-                expect(res).toBeNull();
-              });
-          });
-      });
-  });
-  it('can retrieve a user by :id, delete, and return the delete count', () => {
-    return createUser('music2myEARS', 'YoYo Ma', 'v@v.com')
-      .then(createdUser => {
-        const _id = createdUser._id;
-        return request(app)
-          .delete(`/users/${_id}`)
-          .then(res => {
-            expect(res.body).toEqual({ deleted: 1 });
-            return User.findById(_id)
-              .then(res => {
-                expect(res).toBeNull();
-              });
-          });
-      });
+  describe('DELETE  by :id', () => {
+    it('can retrieve a tweet by :id, delete, and return the delete count', () => {
+      return createTweet('alo')
+        .then(createdTweet => {
+          const _id = createdTweet._id;
+          return request(app)
+            .delete(`/tweets/${_id}`)
+            .then(res => {
+              expect(res.body).toEqual({ deleted: 1 });
+              return Tweet.findById(_id)
+                .then(res => {
+                  expect(res).toBeNull();
+                });
+            });
+        });
+    });
+    it('can retrieve a user by :id, delete, and return the delete count', () => {
+      return createUser('music2myEARS', 'YoYo Ma', 'v@v.com')
+        .then(createdUser => {
+          const _id = createdUser._id;
+          return request(app)
+            .delete(`/users/${_id}`)
+            .then(res => {
+              expect(res.body).toEqual({ deleted: 1 });
+              return User.findById(_id)
+                .then(res => {
+                  expect(res).toBeNull();
+                });
+            });
+        });
+    });
   });
 });
